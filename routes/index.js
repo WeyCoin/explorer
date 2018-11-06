496a89feaeebe0927aab5556858d421a2e2282a2
acidtib <acidtib@protonmail.com>
2018-11-06 15:57:51 -0500

    add masternodelist

--- routes/index.js
+++ routes/index.js
@@ -295,21 +295,26 @@ router.get('/ext/summary', function(req, res) {
       }
     }
     lib.get_hashrate(function(hashrate) {
-      lib.get_connectioncount(function(connections){
-        lib.get_blockcount(function(blockcount) {
-          db.get_stats(settings.coin, function (stats) {
-            if (hashrate == 'There was an error. Check your console.') {
-              hashrate = 0;
-            }
-            res.send({ data: [{
-              difficulty: difficulty,
-              difficultyHybrid: difficultyHybrid,
-              supply: stats.supply,
-              hashrate: hashrate,
-              lastPrice: stats.last_price,
-              connections: connections,
-              blockcount: blockcount
-            }]});
+      lib.get_connectioncount(function(connections) {
+        lib.get_masternodescount(function (masternodestotal) {
+          lib.get_blockcount(function (blockcount) {
+            db.get_stats(settings.coin, function (stats) {
+              if (hashrate == 'There was an error. Check your console.') {
+                hashrate = 0;
+              }
+              res.send({
+                data: [{
+                  difficulty: difficulty,
+                  difficultyHybrid: difficultyHybrid,
+                  supply: stats.supply,
+                  hashrate: hashrate,
+                  lastPrice: stats.last_price,
+                  connections: connections,
+                  masternodesCount: masternodestotal,
+                  blockcount: blockcount
+                }]
+              });
+            });
           });
         });
       });
